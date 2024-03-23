edf043b3dbe17d9576ace1f3eab69b8bb3809020
Bartolomeu Hangalo <bartolomeuhangalo1@gmail.com>
2024-03-23 16:35:58 +0100

    Remove the elevated card from SuccessScreen and adjust image aspect ratio to 16:9

--- app/src/main/java/com/hangalo/spacejam/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/hangalo/spacejam/ui/screens/HomeScreen.kt
@@ -4,12 +4,11 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -87,39 +86,46 @@ fun SuccessScreen(
     astronomicPicture: AstronomicPicture,
     modifier: Modifier = Modifier,
 ) {
-    ElevatedCard(modifier) {
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = CenterHorizontally
-        ) {
-            Text(
-                text = astronomicPicture.title,
-                style = typography.titleLarge,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Start,
+    Column(
+        modifier = modifier.fillMaxWidth(), horizontalAlignment = CenterHorizontally
+    ) {
+        Text(
+            text = astronomicPicture.title,
+            style = typography.titleLarge,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Start,
+            modifier = Modifier
+                .align(Alignment.Start)
+                .padding(8.dp)
+        )
+        Box {
+            Image(
+                painter = painterResource(id = R.drawable.loading_img),
+                contentDescription = stringResource(id = R.string.loading),
+                contentScale = ContentScale.Crop,
                 modifier = Modifier
-                    .align(Alignment.Start)
-                    .padding(8.dp)
+                    .fillMaxWidth()
+                    .aspectRatio(16f / 9f)
             )
             AsyncImage(
                 contentDescription = astronomicPicture.title,
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(astronomicPicture.url).crossfade(true).crossfade(1200)
-                    .build(),
+                model = ImageRequest.Builder(LocalContext.current).data(astronomicPicture.url)
+                    .crossfade(true).crossfade(1200).build(),
                 error = painterResource(id = R.drawable.ic_broken_img),
                 placeholder = painterResource(id = R.drawable.ic_img_placeholder),
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .wrapContentSize(Alignment.Center) // .size(200.dp)
-            )
-            Text(
-                text = astronomicPicture.explanation,
-                style = typography.bodyMedium,
-                fontWeight = FontWeight.Normal,
-                textAlign = TextAlign.Justify,
-                modifier = Modifier.padding(8.dp)
+                    .aspectRatio(16f / 9f)
+                    .padding(8.dp)
             )
         }
+        Text(
+            text = astronomicPicture.explanation,
+            style = typography.bodyMedium,
+            fontWeight = FontWeight.Normal,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(12.dp)
+        )
     }
 }
