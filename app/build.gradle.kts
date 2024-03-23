a09b2ba7974ac448566f1fb3714a00a7cd412057
Bartolomeu Hangalo <bartolomeuhangalo1@gmail.com>
2024-03-23 01:40:38 +0100

    Project setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.10"
     }
     packaging {
         resources {
@@ -51,6 +51,7 @@ android {
 
 dependencies {
 
+    // Compose BoM
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -59,6 +60,17 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // Retrofit
+    implementation(libs.jakewharton.retrofit)
+    implementation(libs.squareup.retrofit2)
+
+    // Coil library and Kotlin serialization plugin
+    implementation(libs.coil)
+    implementation(libs.kotlin.serialization)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+    // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
